b6a28e96f8d3e159c2de628ef462ee96d500b933
Ankit <ankit.998877@gmail.com>
2025-09-15 21:13:15 +0530

    Add files via upload

---
+++ typed-init.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', function(){
+    var typed = new Typed('#typed', {
+        strings: ["Sr. Technical Specialist at Amdocs", "Python Developer", "Web Developer"],
+        typeSpeed: 50,
+        backSpeed: 50,
+        loop: true
+    });
+});
