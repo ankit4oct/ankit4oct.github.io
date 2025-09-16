c56996d95206d9cd36ea7f73c690abf07c92906d
Ankit <ankit.998877@gmail.com>
2025-09-16 20:29:29 +0530

    Delete typed-init.js

--- typed-init.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function(){
-    var typed = new Typed('#typed', {
-        strings: ["Sr. Technical Specialist at Amdocs", "Python Developer", "Web Developer"],
-        typeSpeed: 50,
-        backSpeed: 50,
-        loop: true
-    });
-});
