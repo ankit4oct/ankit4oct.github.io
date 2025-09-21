4bc5efe3d72687b8157266f59425c8564f81d4f1
Ankit <ankit.998877@gmail.com>
2025-09-21 21:25:09 +0530

    Update typed-init.js

--- typed-init.js
+++ typed-init.js
@@ -1,7 +1,7 @@
 
 document.addEventListener('DOMContentLoaded', function(){
     var typed = new Typed('#typed', {
-        strings: ["Sr. Technical Specialist at Amdocs", "Python Developer", "Web Developer"],
+        strings: ["Sr. Technology Specialist at Amdocs", "Python Developer", "Web Developer"],
         typeSpeed: 50,
         backSpeed: 50,
         loop: true
